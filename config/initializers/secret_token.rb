4c0c911e0fb15589af633d18d452c49ee620fe76
Adam Bachman <adam.bachman@gmail.com>
2010-10-31 13:43:40 -0400

    rails default install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keyserve::Application.config.secret_token = '448f2b3696005af0a711baabe92bdc12c8d0cc4a1fd23ed672025986ad1e119b4b70708b911a3365879aabf616ab56582f7b4ead969593515276593b80db1aef'
